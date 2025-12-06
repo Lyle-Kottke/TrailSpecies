8f26c8a244813ffe500234cba39dfb87db0a9bcd
Charles Liu <charlesliu@umass.edu>
2025-12-05 21:43:06 -0500

    Images now load for all found species
    -Images will appear for all found species
    -If no species are found (but no error), displays "no species found on trail"
    -Species results show their common name instead of scientific name

--- trail-species/app/trail/[id]/page.tsx
+++ trail-species/app/trail/[id]/page.tsx
@@ -20,6 +20,7 @@ export default function TrailPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+
   useEffect(() => {
     async function fetchTrail() {
       if (!id) return;
@@ -41,6 +42,7 @@ export default function TrailPage() {
         if (!feature) throw new Error("Trail not found");
 
         const props = feature.properties;
+        //console.log(props)
         setTrail({
           name: props.name,
           trailtype: props.trailtype,
@@ -65,31 +67,39 @@ useEffect(() => {
   const fetchSpecies = async () => {
     try {
       const currentMonth = new Date().getMonth() + 1;
+      console.log(currentMonth, "Current Month!", encodeURIComponent(trail.name), "Trail name!")
 
       const res = await fetch(
         `https://trailgeoapi.onrender.com/species_by_trail?trail_name=${encodeURIComponent(
           trail.name
         )}&current_month=${currentMonth}`
       );
-
       const raw = await res.json();
       const data = typeof raw === "string" ? JSON.parse(raw) : raw;
+      //No results
+      if (data.length == 0) {
+        setLoading(false)
+        setSpecies([])
+        return;
+      }
 
-      const speciesArray = Object.values(data) as string[];
+      const speciesArray = Object.values(data["species_guess"]) as string[];
+      const imageArray = Object.values(data["taxon.default_photo.medium_url"]) as string[];
+      const commnameArray = Object.values(data["taxon.preferred_common_name"])
 
       //convert array into structured objects
       const baseSpecies = speciesArray.map((s, i) => ({
         id: i + 1,
         name: s,
         description: "",
-        image: "",
+        image: imageArray[i],
+        common_name: commnameArray[i] || s
       }));
 
       //remove dupes
       const uniqueSpecies = baseSpecies.filter(
         (s, idx, self) => idx === self.findIndex(t => t.name === s.name)
       );
-
       //wiki lookup for each species
       const enrichedSpecies = await Promise.all(
         uniqueSpecies.map(async (sp) => {
@@ -107,10 +117,10 @@ useEffect(() => {
             return {
               ...sp,
               description: wikiData.extract || "",
-              image:
-                wikiData.originalimage?.source ||
-                wikiData.thumbnail?.source ||
-                "",
+              // image:
+              //   wikiData.originalimage?.source ||
+              //   wikiData.thumbnail?.source ||
+              //   "",
             };
           } catch (err) {
             console.warn("Wikipedia lookup failed:", sp.name, err);
@@ -129,8 +139,10 @@ useEffect(() => {
   fetchSpecies();
 }, [trail]);
 
-
   if (!trail) return <p className="p-8 text-red-600">Trail not found</p>;
+
+  //Captialize helper
+  const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
   return (
     <div className="p-8">
@@ -151,6 +163,8 @@ useEffect(() => {
       </div>
 
       <h2 className="text-2xl font-semibold mb-4">Species Found Here</h2>
+      {species && species.length === 0 && !loading && (
+        <p>No species found at trail</p>)}
       {!species ? (
         <p>Loading species...</p>
       ) : (
@@ -161,15 +175,16 @@ useEffect(() => {
               onClick={() => setSelectedSpecies(s)}
               className="cursor-pointer border rounded-2xl shadow-sm p-4 flex items-center gap-4 hover:shadow-lg transition text-green-600"
             >
-              <Image
-                src={s.image}
-                alt={s.name}
-                width={100}
-                height={120}
-                className="rounded-md object-cover"
-              />
+              <div className="relative w-[100px] h-[100px]">
+                <Image
+                  src={s.image}
+                  alt={s.name}
+                  fill
+                  className="rounded-md object-cover rounded-lg"
+                />
+              </div>
 
-              <h3 className="font-bold text-white">{s.name}</h3>
+              <h3 className="font-bold text-white">{capitalize(s.common_name)}</h3>
             </div>
           ))}
         </div>
