8b669cf64c5119f8abb75495fe82600f78054187
natan <72627669+natkai2040@users.noreply.github.com>
2025-12-09 22:24:45 -0500

    Custom Trails Cannot be Saved

--- trail-species/app/trail/[id]/page.tsx
+++ trail-species/app/trail/[id]/page.tsx
@@ -195,12 +195,14 @@ useEffect(() => {
       </Link>
       <div className="flex flex-col md:flex-row md:items-center justify-center w-full gap-4">
         <h1 className="text-3xl font-bold mt-4">{trail.name}</h1>
-        <button
-          onClick={handleSaveTrail}
-          className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-        >
-          Save Trail
-        </button>
+        { is_custom_trail == false &&  
+          <button
+            onClick={handleSaveTrail}
+            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+          >
+            Save Trail
+          </button>
+        }
       </div>
         
       <p className="text-gray-500 mb-6">{trail?.trailtype}</p>
